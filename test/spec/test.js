86c8bf6676921a1fa104fd95595708eb499f20b6
amygori <amydgori@gmail.com>
2014-07-08 23:59:06 -0400

    tests completed but not passing

--- test/spec/test.js
+++ test/spec/test.js
@@ -20,11 +20,11 @@ var favoritePlaces = [{ city: 'New York', USA: true,},
 
             it('should throw an error when an incorrect argument is passed', function (){
             	/*_.pluck = function(array) {
-							if (!Array.isArray(array) 
-				throw new Error('incorect argument');
+							if (!Array.isArray(array) {
+				throw new Error('incorect argument');}
             	expect(_.pluck()).to.throw(Error); 
             	)};*/
-            	
+            	expect(_.pluck(undefined)).to.throw(Error);
             });  
 
             it('should throw an error when no argument is passed', function () {
@@ -52,13 +52,20 @@ var favoritePlaces = [{ city: 'New York', USA: true,},
             	var kittens = ['Lucia', 'Cosmo', 'Lola', 'Magellan', 'Roo']
             	expect(_.first(kittens)).to.equal('Lucia');
             });
-            it('should throw an error when it is not an array', function () {
-        		var number = !Array;
-        		expect(_.first(number)).to.equal(undefined);
-        		if (!Array.isArray(number)){
-        			throw new Error('Not an array');
+            it('should throw an error when anything other than an array is passed in', function () {
+        		expect(_.first(Array)).to.equal(Array);
+        		_.first= function(array) {
+        		if (!Array.isArray(array))
+        			throw new Error('Not an array!');
         		}
-        		expect(_.first(number)).to.throw(Error);
+        		expect(_.first(!Array)).to.throw(Error);
+      		});
+      		it('should throw an error when no argument is passed in', function (){
+      			_.first = function(argument) {
+							if (!argument) 
+				throw new Error('missing argument');
+            	expect(_.first()).to.throw(Error); 
+            	}
       		});
     	});
     });
@@ -73,6 +80,20 @@ var favoritePlaces = [{ city: 'New York', USA: true,},
             	var kittens = ['Lucia', 'Cosmo', 'Lola', 'Magellan', 'Roo']
             	expect(_.last(kittens)).to.equal('Roo');
             });
+            it('should throw an error when anything other than an array is passed in', function (){
+            	_.last = function(array) {
+            		if (!Array) {
+            		throw new Error('That is not an array');}
+            	}
+            	expect(_.last(!Array)).to.throw(Error);
+            });
+            it('should throw an error when no argument is passed in', function (){
+      			_.last = function(argument) {
+							if (!argument) 
+				throw new Error('missing argument');
+            	expect(_.last()).to.throw(Error); 
+            	}
+      		});
         });
     });
 })();
